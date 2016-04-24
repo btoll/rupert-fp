dadf6c652125a5a11f16316a5017469ea0d02d50
Benjamin Toll <benjam72@yahoo.com>
2016-04-24 11:02:04 -0400

    Addes support for capturing free variables and flagging impure functions

--- src/visitor.js
+++ src/visitor.js
@@ -10,63 +10,108 @@ const list = new Set([
 
 const isLoopStatement = type => list.has(type);
 
-const capture = (node, parent, results) =>
+const captureLoops = (node, parent, results) =>
     results.push({
         node,
         type: 'DontUseLoops'
     });
 
+const captureManager = (() => {
+    const stack = [];
+    let capturing = false;
+
+    return {
+        init(v) {
+            stack.push({
+                arguments: [],
+                params: v ?
+                    v.split(', ').reduce((acc, curr) => {
+                        acc.push(curr);
+                        return acc;
+                    }, []) :
+                [],
+                free: null
+            });
+
+            capturing = true;
+        },
+        capture(v) {
+            if (v === null) {
+                const ctx = stack.pop();
+                capturing = false;
+
+                ctx.free = ctx.arguments.filter(v => ctx.params.indexOf(v) === -1);
+
+                return ctx;
+            } else {
+                if (capturing) {
+                    const ctx = stack.pop();
+
+                    ctx.arguments.push(v);
+                    stack.push(ctx);
+                }
+            }
+        }
+    };
+})();
+
+const captureFreeVariables = function (node, parent, results) {
+    const bodies = node.body.body;
+
+    captureManager.init(getParams(node.params));
+
+    if (bodies && Array.isArray(bodies)) {
+        const type = bodies[0].type;
+
+        // We don't want to capture the node if it's a loop statement or IfStatement.
+        if (bodies.length === 1 && !(isLoopStatement(type) || type === 'IfStatement')) {
+            results.push({
+                node: parent,
+                type: 'UnnecessaryBraces'
+            });
+        }
+
+        bodies.forEach(body => this.visit(body, node, results));
+    } else {
+        if (node.body.type === 'CallExpression' && compareParams(node, node.body)) {
+            results.push({
+                node: node,
+                type: 'UnnecessaryFunctionNesting'
+            });
+        }
+
+        this.visit(node.body, node, results);
+    }
+
+    const ctx = captureManager.capture(null);
+    if (ctx.free.length) {
+        results.push({
+            node,
+            type: 'ImpureFunction'
+        });
+    }
+};
+
 const compareParams = (caller, callee) =>
-    getParams(caller.params).indexOf(getParams(callee.arguments || callee.params)) === 0;
+    callee.params && getParams(caller.params).indexOf(getParams(callee.params)) === 0;
 
 const getParams = params =>
     params.map(arg => arg.name).join(', ');
 
 module.exports = {
-    ArrowFunctionExpression(node, parent, results) {
-        const bodies = node.body.body;
+    ArrowFunctionExpression: captureFreeVariables,
+    FunctionExpression: captureFreeVariables,
 
-        if (bodies && Array.isArray(bodies)) {
-            const type = bodies[0].type;
+    ForStatement: captureLoops,
+    ForInStatement: captureLoops,
+    ForOfStatement: captureLoops,
+    DoWhileStatement: captureLoops,
+    WhileStatement: captureLoops,
 
-            // We don't want to capture the node if it's a loop statement or IfStatement.
-            if (bodies.length === 1 && !(isLoopStatement(type) || type === 'IfStatement')) {
-                results.push({
-                    node: parent,
-                    type: 'UnnecessaryBraces'
-                });
-            }
-
-            bodies.forEach(body => this.visit(body, node, results));
-        } else {
-            this.visit(node.body, node, results);
+    Identifier(node, parent) {
+        if (parent.type !== 'VariableDeclaration') {
+            captureManager.capture(node.name);
         }
-    },
-
-    CallExpression(node, parent, results) {
-        const body = parent.body.body;
-
-        if (body && body.length === 1 && compareParams(parent, node)) {
-            results.push({
-                node: parent,
-                type: 'UnnecessaryFunctionNesting'
-            });
-        }
-
-        // TODO
-        const callArgs = node.arguments;
-
-        if (callArgs.length) {
-            callArgs.forEach(node => this.visit(node, node, results));
-        } else if (node.callee) {
-            this.visit(node.callee, node, results);
-        }
-    },
-
-    ForStatement: capture,
-    ForInStatement: capture,
-    ForOfStatement: capture,
-    DoWhileStatement: capture,
-    WhileStatement: capture
+    }
 };
 
